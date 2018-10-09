50f18cb4940f0abf6eea25a2ebd360a7d06f600f
Julius Park <juliusgeo@gmail.com>
2018-10-08 22:51:23 -0400

    Initial Commit

---
+++ Rida/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rida
+//
+//  Created by Julius Park on 10/8/18.
+//  Copyright © 2018 Julius Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
