bab13bbe8f827efe2383eaa851892c1d05fe2896
Julius Park <juliusgeo@gmail.com>
2018-10-09 11:14:14 -0400

    adding overlay functions

--- Rida/ViewController.swift
+++ Rida/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 import MapKit
+class Point {
+    let coord: CLLocation
+    let time: Float64
+    init(coord: CLLocation){
+        self.coord = coord
+        self.time = Date().timeIntervalSinceReferenceDate
+    }
+}
+class Ride {
+    var points: Array<Point>
+    let creationTime: Float64
+    init(){
+        self.points = Array<Point>()
+        self.creationTime = Date().timeIntervalSinceReferenceDate
+    }
+}
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     //Properties
     @IBOutlet weak var Map: MKMapView!
-
+    var isRecording = false
+    var curRide:Ride? = nil
     @IBAction func recordButton(_ sender: UIButton, forEvent event: UIEvent) {
-        
-        
+        curRide = Ride()
+        isRecording = true
     }
     var locationManager: CLLocationManager = CLLocationManager()
     
@@ -36,11 +53,20 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lastLocation: CLLocation = locations[locations.count - 1]
         animateMap(lastLocation)
+        drawLine(line: (curRide?.points)!)
+        
     }
     
     func animateMap(_ location: CLLocation) {
         let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
         Map.setRegion(region, animated: true)
+    }
+    func drawLine(line: Array<Point>){
+        var coords = Array<CLLocationCoordinate2D>()
+        for point in line {
+            coords.append(point.coord.coordinate as CLLocationCoordinate2D)
+        }
+        Map.addOverlay(MKPolyline.init(coordinates: &coords, count: coords.count))
     }
     
 
