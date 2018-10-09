468859053aa385bc59de3d8a9a1f7661531c7c11
Julius Park <juliusgeo@gmail.com>
2018-10-08 23:36:52 -0400

    initial commit

--- Rida/ViewController.swift
+++ Rida/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    //Properties
+    @IBOutlet weak var Map: MKMapView!
 
-class ViewController: UIViewController {
-
+    @IBAction func recordButton(_ sender: UIButton, forEvent event: UIEvent) {
+        
+        
+    }
+    var locationManager: CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        Map.showsUserLocation = true
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation: CLLocation = locations[locations.count - 1]
+        animateMap(lastLocation)
+    }
+    
+    func animateMap(_ location: CLLocation) {
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        Map.setRegion(region, animated: true)
+    }
+    
 
 }
 
